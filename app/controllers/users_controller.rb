2bd713e99204069f658a623d8dc246a48679bca6
John Phelps <john@fractograf.com>
2017-03-19 15:52:15 -0400

    Reworked to use form_tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,12 @@ class UsersController < ApplicationController
   end
 
   def create
-=begin
     @user = User.new(username: params[:username],
                      email:    params[:email],
                      password: params[:password])
-=end
+=begin
     @user = User.new(user_params)
+=end
     if @user.save
       redirect_to new_user_path
     else
