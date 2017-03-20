750eb7c8e0eb0298dd08840f85fa2c035b3bcd2a
John Phelps <john@fractograf.com>
2017-03-20 14:47:58 -0400

    Reworked to user form_for

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,16 @@
 class UsersController < ApplicationController
 
   def new
-    logger.debug {"User#new"}
+    @user = User.new
   end
 
   def create
+=begin
     @user = User.new(username: params[:username],
                      email:    params[:email],
                      password: params[:password])
-=begin
-    @user = User.new(user_params)
 =end
+    @user = User.new(user_params)
     if @user.save
       redirect_to new_user_path
     else
