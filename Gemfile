a938875618b599b1efcf3b5fe1f8f39c8f19ad9b
John Phelps <john@fractograf.com>
2017-03-21 12:01:52 -0400

    Added index/add/edit and error messages

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ group :development, :test do
 end
 
 group :development do
+  gem 'pry-rails'
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '~> 3.0.5'
   gem 'spring'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    # Seems like more work than necessary to avoid losing an existing
+    # password, but I can't see any other obvious way to do it. Besides,
+    # this needs a password confirmation, but didn't feel like setting it
+    # up right at this time.
+    if params[:user][:password].length == 0
+       params[:user][:password] = @user.password
+    end
+    #binding.pry
+    if @user.update(user_params)
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @users = User.all
+  end
+
   private
     def user_params
       params.require(:user).permit(:username, :email, :password)
